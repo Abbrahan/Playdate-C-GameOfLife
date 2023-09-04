39ca4de422c673b5f26541698627f828eba83256
Jack-Abbrahan <jackcatchpole.jc@gmail.com>
2023-09-04 18:06:32 +1000

    Tinkering with dummy project

--- src/main.c
+++ src/main.c
@@ -12,8 +12,18 @@
 #include "pd_api.h"
 
 static int update(void* userdata);
+static int swapDarkMode(void* userdata, PDMenuItem* menuItem);
 const char* fontpath = "/System/Fonts/Asheville-Sans-14-Bold.pft";
 LCDFont* font = NULL;
+
+#define TEXT_WIDTH 86
+#define TEXT_HEIGHT 16
+
+int x = (LCD_COLUMNS-TEXT_WIDTH)/2;
+int y = (LCD_ROWS-TEXT_HEIGHT)/2;
+int dx = 1;
+int dy = 2;
+int darkModeValue = 0;
 
 #ifdef _WINDLL
 __declspec(dllexport)
@@ -22,13 +32,15 @@ int eventHandler(PlaydateAPI* pd, PDSystemEvent event, uint32_t arg)
 {
 	(void)arg; // arg is currently only used for event = kEventKeyPressed
 
-	if ( event == kEventInit )
-	{
+	if ( event == kEventInit ) {
 		const char* err;
 		font = pd->graphics->loadFont(fontpath, &err);
 		
-		if ( font == NULL )
+		if ( font == NULL ) {
 			pd->system->error("%s:%i Couldn't load font %s: %s", __FILE__, __LINE__, fontpath, err);
+		}
+		
+		PDMenuItem* darkMode = pd->system->addCheckmarkMenuItem("Dark Mode", darkModeValue, swapDarkMode(pd, darkMode), pd);
 
 		// Note: If you set an update callback in the kEventInit handler, the system assumes the game is pure C and doesn't run any Lua code in the game
 		pd->system->setUpdateCallback(update, pd);
@@ -37,15 +49,6 @@ int eventHandler(PlaydateAPI* pd, PDSystemEvent event, uint32_t arg)
 	return 0;
 }
 
-
-#define TEXT_WIDTH 86
-#define TEXT_HEIGHT 16
-
-int x = (400-TEXT_WIDTH)/2;
-int y = (240-TEXT_HEIGHT)/2;
-int dx = 1;
-int dy = 2;
-
 static int update(void* userdata)
 {
 	PlaydateAPI* pd = userdata;
@@ -53,6 +56,17 @@ static int update(void* userdata)
 	pd->graphics->clear(kColorWhite);
 	pd->graphics->setFont(font);
 	pd->graphics->drawText("Hello World!", strlen("Hello World!"), kASCIIEncoding, x, y);
+
+	darkModeValue = pd->system->getMenuItemValue(menuItem);
+
+	if(darkModeValue == 1) {
+		pd->graphics->drawText("On", strlen("On"), kASCIIEncoding, 20, 0);
+	}
+	else {
+		pd->graphics->drawText("Off", strlen("Off"), kASCIIEncoding, 20, 0);
+	}
+
+
 
 	x += dx;
 	y += dy;
@@ -66,5 +80,14 @@ static int update(void* userdata)
 	pd->system->drawFPS(0,0);
 
 	return 1;
+}
+
+static int swapDarkMode(void* userdata, PDMenuItem* menuItem)
+{
+	PlaydateAPI* pd = userdata;
+
+	darkModeValue = pd->system->getMenuItemValue(menuItem);
+
+	pd->display->setInverted(darkModeValue);
 }
 
